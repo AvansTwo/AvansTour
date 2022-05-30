ec59e9cbf72a921d5ab58993cdc2202518bb13e5
Sybrand Bos <94383232+Sybrandbos2001@users.noreply.github.com>
2022-05-30 17:40:21 +0200

    Created homepage with header and footer (#2)
    * edited app.blade with header and footer
    
    * added fonts + CSS file
    
    * created homepage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,70 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>AvansTour | @yield('title')</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/436d2e61dc.js" crossorigin="anonymous"></script>
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Istok+Web:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#"><img src="{{ asset('img/logo_avans.png') }}" alt=""></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item custom-link">
+                    <a class="nav-link active" href="#">Home</a>
+                </li>
+                <li class="nav-item custom-link">
+                    <a class="nav-link" href="#">Speurtochten</a>
+                </li>
+                <li class="nav-item custom-link">
+                    <a class="nav-link" href="#">Scoreboard</a>
+                </li>
+                <li class="nav-item custom-link">
+                    <a class="nav-link" href="#">Over ons</a>
+                </li>
+            </ul>
         </div>
-    </body>
+    </div>
+</nav>
+<main>
+    @yield('content')
+</main>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <a href="#" target="_blank"><i id="facebook" class="fa-brands fa-facebook footer-icon"></i></a>
+                <a href="#" target="_blank"><i id="twitter" class="fa-brands fa-twitter footer-icon"></i></a>
+                <a href="#" target="_blank"><i id="mail" class="fa-solid fa-envelope footer-icon"></i></a>
+            </div>
+            <div class="col-12 text-center">
+                <p id="footer-copyright">&copyAvansTour</p>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
 </html>
