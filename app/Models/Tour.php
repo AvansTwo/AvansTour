256ba54b02beb6f449e3e9bc43f25e972c8b773c
Thimo Luijsterburg <Thimo.Luijsterburg@rietveld.nl>
2022-05-31 11:24:14 +0200

    Start with model relations

--- app/Models/Tour.php
+++ app/Models/Tour.php
@@ -4,8 +4,43 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Tour extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'category_id',
+        'user_id'
+    ];
+
+    public function team(): HasMany
+    {
+        return $this->hasMany(Team::class);
+    }
+
+    public function category(): HasOne
+    {
+        return $this->hasOne(Category::class);
+    }
+
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class);
+    }
+
+    public function tour(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function question(): HasMany
+    {
+        return $this->hasMany(Question::class);
+    }
 }
