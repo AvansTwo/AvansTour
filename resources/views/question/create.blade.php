6e43c59e1137031a8e12f9ed30608a7296e9c6ca
Jelmer1220000 <jjp.verhoeven@student.avans.nl>
2022-06-02 14:32:20 +0200

    quiz.end html/css added

--- resources/views/question/create.blade.php
+++ resources/views/question/create.blade.php
@@ -8,11 +8,6 @@
 
         <select class="form-select" aria-label="Default select example">
             <option selected>Open this select menu</option>
-            <!-- <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option> -->
-
-
 </select>
 
 
@@ -69,4 +64,3 @@
 
 
 @endsection
-<button class="btn primary-btn mb-4" id="tourSubmitButton" type="submit" onclick="FormRequiredFields()">Aanmaken</button>Testdd( -->
