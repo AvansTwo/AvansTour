98e513fd954c2ffe1ce61e63e1ea981febcbc1da
Thimo Luijsterburg <Thimo.Luijsterburg@rietveld.nl>
2022-06-02 14:37:43 +0200

    validation rules

--- app/Http/Requests/Question/StoreQuestionRequest.php
+++ app/Http/Requests/Question/StoreQuestionRequest.php
@@ -24,7 +24,8 @@ class StoreQuestionRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => 'required|max:255',
+            'description' => 'required',
         ];
     }
 }
