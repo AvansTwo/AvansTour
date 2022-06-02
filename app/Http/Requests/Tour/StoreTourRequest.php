00ed82df2fe8a95c84faa77f4e80fde5a823921d
Thimo Luijsterburg <Thimo.Luijsterburg@rietveld.nl>
2022-06-02 15:24:29 +0200

    validation for create tour

--- app/Http/Requests/Tour/StoreTourRequest.php
+++ app/Http/Requests/Tour/StoreTourRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Tour;
 
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -24,7 +24,12 @@ class StoreTourRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'string|required|max:255',
+            'description' => 'string|required|max:255',
+            'image_url' => 'file|nullable',
+            'location' => 'nullable',
+            'category_id' => '',
+            'user_id' => ''
         ];
     }
 }
