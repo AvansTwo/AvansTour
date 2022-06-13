20edef2da6492065f3e236c690e4d9c5fb131be5
Sybrand Bos <snjw.bos@student.avans.nl>
2022-06-13 19:21:14 +0200

    Dashboard route

--- routes/web.php
+++ routes/web.php
@@ -88,3 +88,10 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+//Dashboard
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
