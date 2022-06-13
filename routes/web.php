944987464fc70b3d34386357662cff659e1c87f8
Sybrand <snjw.bos@student.avans.nl>
2022-06-13 12:54:12 +0200

    Laravel login fixed

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\TourController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\QuizController;
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,46 +28,49 @@ Route::get('/speurtochten/categorie/{id}', [TourController::class, 'categoryFilt
 Route::get('/speurtochten', [TourController::class, 'index']);
 
 //Tour create
-Route::post('/speurtochten/aanmaken', [TourController::class, 'store']);
-Route::get('/speurtochten/aanmaken', [TourController::class, 'create']);
-
-Route::get('/answerMap/{id}', [QuestionController::class, 'answerMap']);
+Route::post('/speurtochten/aanmaken', [TourController::class, 'store'])->middleware(['auth']);
+Route::get('/speurtochten/aanmaken', [TourController::class, 'create'])->middleware(['auth']);
 
 //Tour edit
-Route::post('/speurtochten/aanpassen/{id}', [TourController::class, 'update']);
-Route::get('/speurtochten/aanpassen/{id}', [TourController::class, 'edit']);
+Route::post('/speurtochten/aanpassen/{id}', [TourController::class, 'update'])->middleware(['auth']);
+Route::get('/speurtochten/aanpassen/{id}', [TourController::class, 'edit'])->middleware(['auth']);
 
 //Tour show
 Route::get('/speurtochten/{id}', [TourController::class, 'show']);
 
 //Tour delete
-Route::get('/speurtochten/verwijderen/{id}', [TourController::class, 'destroy']);
+Route::get('/speurtochten/verwijderen/{id}', [TourController::class, 'destroy'])->middleware(['auth']);
 
 //Question create
-Route::post('/speurtochten/{id}/vragen/aanmaken', [QuestionController::class, 'store']);
-Route::get('/speurtochten/{id}/vragen/aanmaken', [QuestionController::class, 'create']);
+Route::post('/speurtochten/{id}/vragen/aanmaken', [QuestionController::class, 'store'])->middleware(['auth']);
+Route::get('/speurtochten/{id}/vragen/aanmaken', [QuestionController::class, 'create'])->middleware(['auth']);
 
 //Question show
-Route::get('/vragen/{id}', [QuestionController::class, 'show']);
+Route::get('/vragen/{id}', [QuestionController::class, 'show'])->middleware(['auth']);
 
 //Question edit
-Route::post('/vragen/aanpassen/{id}', [QuestionController::class, 'update']);
-Route::get('/vragen/aanpassen/{id}', [QuestionController::class, 'edit']);
+Route::post('/vragen/aanpassen/{id}', [QuestionController::class, 'update'])->middleware(['auth']);
+Route::get('/vragen/aanpassen/{id}', [QuestionController::class, 'edit'])->middleware(['auth']);
 
 //Question delete
-Route::get('/vragen/verwijderen/{id}', [QuestionController::class, 'destroy']);
+Route::get('/vragen/verwijderen/{id}', [QuestionController::class, 'destroy'])->middleware(['auth']);
 
 //Quiz create
-Route::post('/quiz/aanmaken', [QuizController::class, 'store']);
-Route::get('/speurtochten/{id}/quiz', [QuizController::class, 'create']);
+Route::post('/quiz/aanmaken', [QuizController::class, 'store'])->middleware(['auth']);
+Route::get('/speurtochten/{id}/quiz', [QuizController::class, 'create'])->middleware(['auth']);
 
 //Quiz play mapselect page
 Route::get('/quiz/spelen/{teamHash}', [QuizController::class, 'getRemainingQuestions']);
 
-//Quiz play get question 
+//Quiz play get question
 Route::get('/quiz/spelen/{teamHash}/vraag/{questionId}', [QuizController::class, 'getQuestion']);
 
-//Quiz play store answer question 
+//Quiz play store answer question
 Route::post('/quiz/spelen/{teamHash}/vraag/{questionId}/beantwoorden', [QuizController::class, 'storeTeamProgress']);
 
 //Dashboard
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
