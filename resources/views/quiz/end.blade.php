caaccda9ec7dd6a46d711307bb676e22f9467f67
Joost <ja.vandam3@student.avans.nl>
2022-06-13 13:50:49 +0200

    Tour ending page styled

--- resources/views/quiz/end.blade.php
+++ resources/views/quiz/end.blade.php
@@ -2,43 +2,54 @@
 @section('title', 'Quiz aanmaken')
 @section('content')
 
-@extends('layouts.app')
-@section('title', 'Quiz aanmaken')
-@section('content')
-
 <div class="container">
-    <div class="col-12">
-        <h1 class="my-5 text-center">Quiz</span> beëindigd!</h1>
-    </div>
-    <div class="py-5 grey-bg col-12">
-        <div class="row quiz-title">
-            <div class="d-flex mt-3">
-                <i class="fa-solid fa-star quiz-icon col-3"></i>
-                <h1 class="my-5 text-center col-4">Team <span class="title-colored">{{$team->team_name}}</span></h1>
-                <i class="fa-solid fa-star quiz-icon col-3"></i>
-            </div>
+
+    <div class="row">
+
+        <div class="col-12">
+            <h1 class="my-5 text-center">Tour</span> beëindigd!</h1>
         </div>
-        <hr>
-        <h2 class="quiz-info"> Punten: <span class="quiz-colored">{{$points}}</span></h2>
-        <h2 class="quiz-info"> Vragen beantwoord: <span class="quiz-colored">{{$teamQuestion}}</span></h2>
-        <h2 class="quiz-info"> Minuten over gedaan: <span class="quiz-colored">{{$difference}}</span></h2>
-        <hr>
-        <div class="row quiz-title">
-            <div class="d-flex mt-3">
-                <i class="fa-solid fa-star quiz-icon col-3"></i>
-                <h1 class="my-5 text-center col-4">Team <span class="title-colored">{{$team->team_name}}</span></h1>
-                <i class="fa-solid fa-star quiz-icon col-3"></i>
+
+        <div class="row grey-bg p-5">
+
+            <div class="col-12 col-lg-8">
+
+                <div class="col-12 quiz-title d-flex">
+                        <h1 class="my-5">Team: <span class="title-colored">{{$team->team_name}}</span></h1>
+                </div>
+
+                <div class="col-12 d-flex mt-4">
+                    <i class="fa-solid fa-award question-icon-mark"></i>
+                    <h2 class="quiz-info">Punten: <span class="quiz-colored">{{$points}}</span></p>
+                </div>
+
+                <div class="col-12 d-flex mt-4">
+                    <i class="fa-solid fa-certificate question-icon-mark"></i>
+                    <h2 class="quiz-info">Vragen beantwoord: <span class="quiz-colored">{{$teamQuestion}}</span></h2>
+                </div>
+
+                <div class="col-12 d-flex mt-4">
+                    <i class="fa-solid fa-hourglass question-icon-mark"></i>
+                    <h2 class="quiz-info">Minuten over gedaan: <span class="quiz-colored">{{$difference}}</span></h2>
+                </div>
+
+                <div class="quiz-title mt-4">
+                    <a class="btn primary-btn" href="/scoreboard"><i class="fa-solid fa-ranking-star"></i> Scoreboard</a>
+                </div>
 
             </div>
+
+            <div class="col-12 col-lg-4">
+                <img class="img-fluid" src="{{ asset('img/man.png') }}" alt="landing-image">
+            </div>
+
+
         </div>
-        <div class="quiz-title">
-            <a class="btn primary-btn" href="/speurtochten/1"><i class="fa-solid fa-chevron-left"></i> Ga
-                terug</a>
-        </div>
+
+        {{-- <div class="col-12 col-lg-5">
+            <img class="img-fluid" src="{{ asset('img/landing_img.png') }}" alt="landing-image">
+        </div> --}}
     </div>
 </div>
-
-
-@endsection
 
 @endsection
