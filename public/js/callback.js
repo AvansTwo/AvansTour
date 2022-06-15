b0ffda2cf30570ca7d4f983b5c7983202a9a61bc
Tristan Goossens <tt.goossens@student.avans.nl>
2022-06-15 11:30:15 +0200

    Map click functionality and added custom map marker icon
    add marker icon for leaflet map
    
    add map click callback
    
    
    add icon to leaflet map
    
    
    import leaflet component in crud pages
    
    
    added map click callback functionality

--- public/js/callback.js
+++ public/js/callback.js
@@ -5,20 +5,26 @@ function markerClick(question_id, team_hash) {
     document.location.href = `/quiz/spelen/${team_hash}/vraag/${question_id}`;
 }
 
-function mapPickLocation(location){
-    let locationText = document.getElementById('tourStartLocation') || document.getElementById('questionLocation');
-    if (locationText) {
-    let format = location.toString().slice(7, -1)
-    let strings = format.split(',')
-    let formatted = strings[0] + "," + strings[1].slice(1, strings[1].length);
-    locationText.value = formatted;
+function mapPickLocation(map, marker, event, circle) {
+    var d = map.distance(event.latlng, circle.getLatLng());
+    if (d < circle.getRadius()) {
+        marker.setLatLng(new L.LatLng(event.latlng.lat, event.latlng.lng));
 
-    document.getElementById("locationChanged").classList.remove("d-none");
-    setTimeout(function(){
-        document.getElementById("locationChanged").classList.add("d-none");
- }, 3000);
-}
-
+        let location = event.latlng;
+        let locationText = document.getElementById('tourStartLocation') || document.getElementById('questionLocation');
+        if (locationText) {
+            let format = location.toString().slice(7, -1)
+            let strings = format.split(',')
+            let formatted = strings[0] + "," + strings[1].slice(1, strings[1].length);
+            locationText.value = formatted;
+            document.getElementById("locationChanged").classList.remove("d-none");
+            setTimeout(function () {
+                document.getElementById("locationChanged").classList.add("d-none");
+            }, 3000);
+        }
+    } else {
+        alert('Place your location within the circle!');
+    }
 }
 
 let MapIsInvisible = false;
@@ -31,9 +37,9 @@ function showMap() {
         map.disabled = true;
         MapIsInvisible = true;
     } else {
-       map.classList.remove("d-none");
-       map.disabled = false;
-       MapIsInvisible = false;
+        map.classList.remove("d-none");
+        map.disabled = false;
+        MapIsInvisible = false;
     }
 
 }
