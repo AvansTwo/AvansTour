cd63687ab91534d7ae4e3b650acb0e61e95f8b4e
Tristan Goossens <tt.goossens@student.avans.nl>
2022-06-22 17:04:26 +0200

    rework dashboard page

--- database/migrations/2022_06_07_111245_create_team_progress_table.php
+++ database/migrations/2022_06_07_111245_create_team_progress_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::create('team_progress', function (Blueprint $table) {
-            $table->id();
+            $table->primary(['team_id', 'question_id']);
+            //Table properties
             $table->foreignId('team_id')->constrained('team')->cascadeOnDelete();
             $table->foreignId('question_id')->constrained('question')->cascadeOnDelete();
             $table->foreignId('team_answer_id')->constrained('team_answer')->cascadeOnDelete();
