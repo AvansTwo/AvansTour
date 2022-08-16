d0f40d181aa46fa2277668ce8170e0ea2b03d4d9
Tristan Goossens <tt.goossens@student.avans.nl>
2022-08-16 14:31:47 +0200

    added search and status dot color to dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -26,7 +26,7 @@ class DashboardController extends Controller
         $teams = DB::table('team')
                     ->join('team_progress', 'team.id', '=', 'team_progress.team_id')
                     ->join('tour', 'tour.id', '=', 'team.tour_id')
-                    ->select('team.team_name', 'team.id', 'tour.name')
+                    ->select('team.team_name', 'team.id', 'tour.name','team.end_time')
                     ->where('team_progress.status', '=', "Afwachting")
                     ->whereNotNull('team_progress.team_id')
                     ->groupBy('team.team_name', 'team.id', 'tour.name')
@@ -64,6 +64,55 @@ class DashboardController extends Controller
 
         
 
+        return view('dashboard.index')->with('teams', $teams);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function filterName(Request $request)
+    {
+        $teams = DB::table('team')
+                    ->join('team_progress', 'team.id', '=', 'team_progress.team_id')
+                    ->join('tour', 'tour.id', '=', 'team.tour_id')
+                    ->select('team.team_name', 'team.id', 'tour.name', 'team.end_time')
+                    ->where('team_progress.status', '=', "Afwachting")
+                    ->where('team.team_name', 'LIKE', '%'.$request->teamString.'%')
+                    ->whereNotNull('team_progress.team_id')
+                    ->groupBy('team.team_name', 'team.id', 'tour.name')
+                    ->paginate(3);
+
+        foreach($teams as $team) {
+            $teamProgress = DB::select(DB::raw("SELECT * 
+            FROM team_progress AS tp
+            INNER JOIN tour_question AS tq ON tp.question_id = tq.question_id
+            INNER JOIN question AS q ON tq.question_id = q.id
+            INNER JOIN tour AS t ON tq.tour_id = t.id
+            WHERE tp.status = 'Afwachting'
+            AND tp.team_id = :team_id
+            AND t.name = :tour_name;"
+            ), array(
+                'team_id' => $team->id,
+                'tour_name' => $team->name,
+            ));
+
+            $team->progress = TeamProgress::hydrate($teamProgress);
+
+            foreach($team->progress as $progress){
+                $progress->answer = TeamAnswer::find($progress->team_answer_id);
+
+                if($progress->answer->is_file){
+                    $progress->answer->answer = StorageController::get($progress->answer->answer);
+                }
+
+                if($progress->question->image_url != null){
+                    $progress->question->image_url = StorageController::get($progress->question->image_url);
+                }
+            }
+        } 
+            
         return view('dashboard.index')->with('teams', $teams);
     }
 
